838fb48776aba83d2101bf6cd0f9f2c2d076b747
Anupam Jindal <anu_jindal0001@yahoo.com>
2012-06-02 19:08:37 -0700

    initial KidGames project structure

---
+++ KidGames/src/kidgames/handlers/LevelHandler.java
@@ -0,0 +1,13 @@
+package kidgames.handlers;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LevelHandler {
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+	{
+		
+	}
+
+}
